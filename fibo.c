faf70e0159f59c821972bf3576b46e574962382a
Aditi Sharma <115368033+ShhAditi@users.noreply.github.com>
2023-09-25 23:32:43 +0530

    Added fibo.c

---
+++ fibo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n,n1=0,n2=1,n3;
+    printf("Enter n:");
+    scanf("%d",&n);
+    printf("0, 1, ");
+    for(int i=3;i<n;i++){
+        n3=n1+n2;
+        n1=n2;
+        n2=n3;
+        printf("%d, ",n3);
+    }
+    return 0;
+}
